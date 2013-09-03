a70c7fa9c11b0483213b5f67495ce729f8600f93
xorrr <derxor@gmail.com>
2013-09-04 01:43:13 +0200

    initial commit

---
+++ src/main/java/earth/xor/App.java
@@ -0,0 +1,13 @@
+package earth.xor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
