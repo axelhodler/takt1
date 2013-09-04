ba78db42893176ee538074a13ccad47ad1a5425f
xorrr <derxor@gmail.com>
2013-09-04 19:20:35 +0200

    pass test implementing grabTitle method with jsoup help

--- src/main/java/earth/xor/TitleGrabber.java
+++ src/main/java/earth/xor/TitleGrabber.java
@@ -1,10 +1,22 @@
 package earth.xor;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 public class TitleGrabber {
 
     public String grabTitle(String url) {
-	// TODO Auto-generated method stub
+
+	Document doc;
+	try {
+	    doc = Jsoup.connect(url).get();
+	    return doc.title();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+
 	return null;
     }
-
 }
