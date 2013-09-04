c1cbe9b11d152011afa49685a7ef0c7979f77a4d
xorrr <derxor@gmail.com>
2013-09-04 21:09:48 +0200

    pass test when dealing with the unknownhostexception

--- src/main/java/earth/xor/TitleGrabber.java
+++ src/main/java/earth/xor/TitleGrabber.java
@@ -1,6 +1,7 @@
 package earth.xor;
 
 import java.io.IOException;
+import java.net.UnknownHostException;
 
 import org.jsoup.Jsoup;
 import org.jsoup.UnsupportedMimeTypeException;
@@ -16,6 +17,8 @@ public class TitleGrabber {
 	    return doc.title();
 	} catch (UnsupportedMimeTypeException e) {
 	    System.out.println("The URL is not a html page");
+	} catch (UnknownHostException e) {
+	    System.out.println("The URL does not lead to an existing resource");
 	} catch (IOException e) {
 	    e.printStackTrace();
 	}
