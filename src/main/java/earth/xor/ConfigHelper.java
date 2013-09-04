59ee63cdc04104ba18520b8e4bb0802ae7e9824a
xorrr <derxor@gmail.com>
2013-09-04 21:21:35 +0200

    implement getName method of the confighelper

--- src/main/java/earth/xor/ConfigHelper.java
+++ src/main/java/earth/xor/ConfigHelper.java
@@ -1,9 +1,25 @@
 package earth.xor;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConfigHelper {
 
     public String getBotName() {
-	// TODO Auto-generated method stub
+	Properties prop = new Properties();
+	
+	try {
+	    prop.load(new FileInputStream("config.properties"));
+	    return prop.getProperty("botname");
+	} catch (FileNotFoundException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	} catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
 	return null;
     }
 
