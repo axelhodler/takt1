17482b2ed5c57f611946bfdb653a9057ff950431
xorrr <derxor@gmail.com>
2013-09-04 19:23:10 +0200

    fail test when grabbing title of an image because of UnsupportedMimeTypeException

--- src/test/java/earth/xor/TestTitleGrabber.java
+++ src/test/java/earth/xor/TestTitleGrabber.java
@@ -5,16 +5,27 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestTitleGrabber {
-    
+
     private String title = "Regular expression - Wikipedia, the free encyclopedia";
     private String url = "http://en.wikipedia.org/wiki/Regex";
-    
+
+    private String imageUrl = "http://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Thompson-kleene-star.svg/503px-Thompson-kleene-star.svg.png";
+
     @Test
     public void testGrabbingTheTitle() {
 	TitleGrabber tg = new TitleGrabber();
-	
+
 	String grabbedTitle = tg.grabTitle(url);
-	
+
 	assertEquals(title, grabbedTitle);
+    }
+    
+    @Test
+    public void testGrabbingTheTitleWithAnImage() {
+	TitleGrabber tg = new TitleGrabber();
+	
+	String grabbedTitel = tg.grabTitle(imageUrl);
+	
+	assertEquals(null, grabbedTitel);
     }
 }
