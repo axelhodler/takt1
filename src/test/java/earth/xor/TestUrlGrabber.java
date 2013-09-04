f8f968c647cb6ae6adaf6734d3fdfc17aa01aca3
xorrr <derxor@gmail.com>
2013-09-04 19:09:15 +0200

    refactoring

--- src/test/java/earth/xor/TestUrlGrabber.java
+++ src/test/java/earth/xor/TestUrlGrabber.java
@@ -1,36 +1,39 @@
 package earth.xor;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestUrlGrabber {
 
     private String testUrl = "http://en.wikipedia.org/wiki/Regex";
-    private String secondTestUrl = "http://en.wikipedia.org/wiki/Regular_expression";
+    private String testUrlTwo = "http://en.wikipedia.org/wiki/Regular_expression";
     
     private String testSentence = "foo " + testUrl + " bar baz";
-    private String secondTestSentence= "foo bar " + secondTestUrl + " baz";
+    private String testSentenceTwo = "foo bar " + testUrlTwo + " baz";
     
     private String testSentenceWithoutUrl = "foo bar baz";
+
+    private UrlGrabber ug;
+    
+    @Before
+    public void setUpTests() {
+	ug = new UrlGrabber();
+    }
     
     @Test
     public void testGettingTheUrl() {
 	
-	UrlGrabber ug = new UrlGrabber();
-	
 	String url = ug.grabUrl(testSentence);
-	
-	String secondUrl = ug.grabUrl(secondTestSentence);
+	String secondUrl = ug.grabUrl(testSentenceTwo);
 	
 	assertEquals(testUrl, url);
-	assertEquals(secondTestUrl, secondUrl);	
+	assertEquals(testUrlTwo, secondUrl);	
     }
     
     @Test
     public void dealWithNoUrlProvided() {
-	UrlGrabber ug = new UrlGrabber();
-	
 	String url = ug.grabUrl(testSentenceWithoutUrl);
 	
 	assertEquals(null, url);		
