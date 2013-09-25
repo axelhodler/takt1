7c058b1bf3ab21272e29397e45e95d5b2f14426d
xorrr <derxor@gmail.com>
2013-09-25 21:31:12 +0200

    move identify from launchBot into its own method

--- src/main/java/bot/Bot.java
+++ src/main/java/bot/Bot.java
@@ -10,12 +10,13 @@ public class Bot extends ListenerAdapter implements Listener {
     private static ConfigHelper configHelper;
     private TitleGrabber titleGrabber = new TitleGrabber();
     private UrlGrabber urlGrabber = new UrlGrabber();
+    private PircBotX bot;
     
     public void launchBot() throws Exception {
 		
 	configHelper = new ConfigHelper();
 	
-	PircBotX bot = new PircBotX();
+	bot = new PircBotX();
 	
 	bot.setName(configHelper.getBotName());
 	bot.setLogin(configHelper.getIdentName());
@@ -25,10 +26,14 @@ public class Bot extends ListenerAdapter implements Listener {
 	
 	bot.connect(configHelper.getServer());
 	
+	bot.joinChannel(configHelper.getChannel());
+    }
+    
+    // optional
+    public void identifyWithServer(String password) throws InterruptedException {
 	bot.identify(configHelper.getIdentifyPassword());
 	// sleep 5secs before joining the channel
 	Thread.sleep(5000);
-	bot.joinChannel(configHelper.getChannel());
     }
     
     @Override
