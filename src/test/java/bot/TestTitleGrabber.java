e2f9f59dd105a9a1f7dbcdabbc70cfc03e93adb9
xorrr <derxor@gmail.com>
2013-10-20 23:26:25 +0200

    reformat TestTitleGrabber

--- src/test/java/bot/TestTitleGrabber.java
+++ src/test/java/bot/TestTitleGrabber.java
@@ -9,41 +9,38 @@ import bot.TitleGrabber;
 
 public class TestTitleGrabber {
 
-    private String title = "Regular expression - Wikipedia, the free encyclopedia";
+    private String title = "Regular expression - Wikipedia, the "
+            + "free encyclopedia";
     private String url = "http://en.wikipedia.org/wiki/Regex";
-    
+
     private String nonExistingUrl = "http://www.test.test";
 
-    private String imageUrl = "http://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Thompson-kleene-star.svg/503px-Thompson-kleene-star.svg.png";
+    private String imageUrl = "http://upload.wikimedia.org/wikipedia/commons"
+            + "/thumb/8/8e/Thompson-kleene-star.svg/503px-Thompson-kleene-"
+            + "star.svg.png";
 
     private TitleGrabber tg;
-    
+
     @Before
     public void setUp() {
-	tg = new TitleGrabber();
+        tg = new TitleGrabber();
     }
-    
+
     @Test
     public void testGrabbingTheTitle() {
-
-	String grabbedTitle = tg.grabTitle(url);
-
-	assertEquals(title, grabbedTitle);
+        String grabbedTitle = tg.grabTitle(url);
+        assertEquals(title, grabbedTitle);
     }
-    
+
     @Test
     public void testGrabbingTheTitleWithAnImage() {
-	
-	String grabbedTitel = tg.grabTitle(imageUrl);
-	
-	assertEquals(null, grabbedTitel);
+        String grabbedTitel = tg.grabTitle(imageUrl);
+        assertEquals(null, grabbedTitel);
     }
-    
+
     @Test
     public void testGrabbingTheTitleOfANonExistingUrl() {
-	
-	String grabbedTitle = tg.grabTitle(nonExistingUrl);
-	
-	assertEquals(null, grabbedTitle);
+        String grabbedTitle = tg.grabTitle(nonExistingUrl);
+        assertEquals(null, grabbedTitle);
     }
 }
