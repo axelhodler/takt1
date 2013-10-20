73438aeb871be6ca0eadd798066c48411307c780
xorrr <derxor@gmail.com>
2013-10-20 23:40:53 +0200

    reformat the Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,24 +16,24 @@ public class Main extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-	    throws ServletException, IOException {
-	resp.getWriter().print(":)");
+            throws ServletException, IOException {
+        resp.getWriter().print(":)");
     }
 
     public static void main(String[] args) throws Exception {
 
-	Bot bot = new Bot();
-	bot.launchBot();
+        Bot bot = new Bot();
+        bot.launchBot();
 
-	KeepWebappAliveThread t = new KeepWebappAliveThread();
+        KeepWebappAliveThread t = new KeepWebappAliveThread();
 
-	Server server = new Server(Integer.valueOf(System.getenv("PORT")));
-	ServletContextHandler context = new ServletContextHandler(
-		ServletContextHandler.SESSIONS);
-	context.setContextPath("/");
-	server.setHandler(context);
-	context.addServlet(new ServletHolder(new Main()), "/*");
-	server.start();
-	server.join();
+        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+        ServletContextHandler context = new ServletContextHandler(
+                ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(new Main()), "/*");
+        server.start();
+        server.join();
     }
 }
