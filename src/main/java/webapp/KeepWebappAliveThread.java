e0c50fd3600ece2f5e998acd6c240161422c060a
xorrr <derxor@gmail.com>
2013-10-20 23:32:26 +0200

    reformat KeepWebappAliveThread

--- src/main/java/webapp/KeepWebappAliveThread.java
+++ src/main/java/webapp/KeepWebappAliveThread.java
@@ -6,36 +6,36 @@ import java.net.URLConnection;
 
 import bot.ConfigHelper;
 
-public class KeepWebappAliveThread extends Thread{
+public class KeepWebappAliveThread extends Thread {
 
     public KeepWebappAliveThread(String name) {
-	super(name);
-	start();
+        super(name);
+        start();
     }
-    
+
     @Override
     public void run() {
-	
-	ConfigHelper configHelper = new ConfigHelper();
-	
-	URL myurl = null;
-	try {
-	    myurl = new URL(configHelper.getWebappUrl());
-	} catch (MalformedURLException e1) {
-	    e1.printStackTrace();
-	}
-	URLConnection myURLConnection;
-	
-	try {
-	    while (true) {
-		    Thread.sleep(600000);
-		    System.out.println("connect to the webapp then sleep 10min");
-		    myURLConnection = myurl.openConnection();
-		    myURLConnection.connect();
-		    myURLConnection.getContentType();
-		}
-	} catch (Exception e){
-	    e.printStackTrace();
-	}
+
+        ConfigHelper configHelper = new ConfigHelper();
+
+        URL myurl = null;
+        try {
+            myurl = new URL(configHelper.getWebappUrl());
+        } catch (MalformedURLException e1) {
+            e1.printStackTrace();
+        }
+        URLConnection myURLConnection;
+
+        try {
+            while (true) {
+                Thread.sleep(600000);
+                System.out.println("connect to the webapp then sleep 10min");
+                myURLConnection = myurl.openConnection();
+                myURLConnection.connect();
+                myURLConnection.getContentType();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
