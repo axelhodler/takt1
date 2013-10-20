e6ec044881f76314d14283b934f74c206c71ee5f
xorrr <derxor@gmail.com>
2013-10-21 00:17:24 +0200

    reformat and refactor UrlGrabber

--- src/main/java/bot/UrlGrabber.java
+++ src/main/java/bot/UrlGrabber.java
@@ -5,17 +5,23 @@ import java.util.regex.Pattern;
 
 public class UrlGrabber {
 
-    private String urlPattern = "((https?):((//)|(\\\\))+[\\w\\d:#@%/;$()~_?\\+-=\\\\\\.&]*)";
-    
+    private String urlPattern = "((https?):((//)|(\\\\))+[\\w\\d:#@%/;$()"
+            + "~_?\\+-=\\\\\\.&]*)";
+
     public String grabUrl(String sentence) {
-	Pattern p = Pattern.compile(urlPattern);
-	Matcher m = p.matcher(sentence);
-	
-	if (m.find()) {
-	    return m.group(1);
-	}
-	
-	return null;
+        Pattern p = Pattern.compile(urlPattern);
+        Matcher m = p.matcher(sentence);
+
+        String url = null;
+
+        url = ifMessageContainsUrl(m, url);
+        return url;
     }
 
+    private String ifMessageContainsUrl(Matcher m, String url) {
+        if (m.find()) {
+            url = m.group(1);
+        }
+        return url;
+    }
 }
