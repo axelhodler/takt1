aa2179d99ed253f578445e8df7405493afa4973c
xorrr <derxor@gmail.com>
2013-10-20 23:29:18 +0200

    reformat TestUrlGrabber

--- src/test/java/bot/TestUrlGrabber.java
+++ src/test/java/bot/TestUrlGrabber.java
@@ -10,34 +10,32 @@ import bot.UrlGrabber;
 public class TestUrlGrabber {
 
     private String testUrl = "http://en.wikipedia.org/wiki/Regex";
-    private String testUrlTwo = "http://en.wikipedia.org/wiki/Regular_expression";
-    
+    private String testUrlTwo = "http://en.wikipedia.org/wiki/"
+            + "Regular_expression";
+
     private String testSentence = "foo " + testUrl + " bar baz";
     private String testSentenceTwo = "foo bar " + testUrlTwo + " baz";
-    
+
     private String testSentenceWithoutUrl = "foo bar baz";
 
     private UrlGrabber ug;
-    
+
     @Before
     public void setUpTests() {
-	ug = new UrlGrabber();
+        ug = new UrlGrabber();
     }
-    
+
     @Test
     public void testGettingTheUrl() {
-	
-	String url = ug.grabUrl(testSentence);
-	String secondUrl = ug.grabUrl(testSentenceTwo);
-	
-	assertEquals(testUrl, url);
-	assertEquals(testUrlTwo, secondUrl);	
+        String url = ug.grabUrl(testSentence);
+        String secondUrl = ug.grabUrl(testSentenceTwo);
+        assertEquals(testUrl, url);
+        assertEquals(testUrlTwo, secondUrl);
     }
-    
+
     @Test
     public void dealWithNoUrlProvided() {
-	String url = ug.grabUrl(testSentenceWithoutUrl);
-	
-	assertEquals(null, url);		
+        String url = ug.grabUrl(testSentenceWithoutUrl);
+        assertEquals(null, url);
     }
 }
