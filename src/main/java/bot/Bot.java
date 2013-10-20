973a169c96617dc2a563ea3c0891253c504ac7ee
xorrr <derxor@gmail.com>
2013-10-20 23:44:34 +0200

    reformat Bot

--- src/main/java/bot/Bot.java
+++ src/main/java/bot/Bot.java
@@ -5,47 +5,48 @@ import org.pircbotx.hooks.Listener;
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.MessageEvent;
 
+@SuppressWarnings("rawtypes")
 public class Bot extends ListenerAdapter implements Listener {
-    
+
     private static ConfigHelper configHelper;
     private TitleGrabber titleGrabber = new TitleGrabber();
     private UrlGrabber urlGrabber = new UrlGrabber();
     private PircBotX bot;
-    
-    public void launchBot() throws Exception {
-		
-	configHelper = new ConfigHelper();
-	
-	bot = new PircBotX();
-	
-	bot.setName(configHelper.getBotName());
 
-	bot.setVerbose(true);
-	
-	bot.getListenerManager().addListener(new Bot());
-	
-	bot.connect(configHelper.getServer());
-	
-	bot.joinChannel(configHelper.getChannel());
+    public void launchBot() throws Exception {
+
+        configHelper = new ConfigHelper();
+
+        bot = new PircBotX();
+
+        bot.setName(configHelper.getBotName());
+
+        bot.setVerbose(true);
+
+        bot.getListenerManager().addListener(new Bot());
+
+        bot.connect(configHelper.getServer());
+
+        bot.joinChannel(configHelper.getChannel());
     }
-    
+
     // optional
     public void identifyWithServer(String password) throws InterruptedException {
-	bot.identify(configHelper.getIdentifyPassword());
-	// sleep 5secs before joining the channel
-	Thread.sleep(5000);
+        bot.identify(configHelper.getIdentifyPassword());
+        // sleep 5secs before joining the channel
+        Thread.sleep(5000);
     }
-    
+
     @Override
     public void onMessage(MessageEvent event) throws Exception {
-	String message = event.getMessage();
-	String url = urlGrabber.grabUrl(message);
-	
-	if (url != null) {
-	    String title = titleGrabber.grabTitle(url);
-	    if (title != null) {
-		event.getBot().sendMessage(event.getChannel(), title);
-	    }
-	}
+        String message = event.getMessage();
+        String url = urlGrabber.grabUrl(message);
+
+        if (url != null) {
+            String title = titleGrabber.grabTitle(url);
+            if (title != null) {
+                event.getBot().sendMessage(event.getChannel(), title);
+            }
+        }
     }
 }
