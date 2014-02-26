e16da61c9e039004c674599829ef13ce03c91634
xorrr <derxor@gmail.com>
2014-02-27 00:53:16 +0100

    refactor BotHandler

--- src/main/java/bot/BotHandler.java
+++ src/main/java/bot/BotHandler.java
@@ -20,10 +20,20 @@ public class BotHandler extends ListenerAdapter {
     public void onGenericMessage(final GenericMessageEvent event)
             throws Exception {
         String url = ug.grabUrl(event.getMessage());
-        if (url != null) {
+
+        if (isNotNull(url)) {
             String title = tg.grabTitle(url);
-            if (title != null)
-                event.respond(title);
+            checkIfRespondWithTitle(event, title);
         }
+    }
+
+    private void checkIfRespondWithTitle(final GenericMessageEvent event,
+            String title) {
+        if (isNotNull(title))
+            event.respond(title);
+    }
+
+    private boolean isNotNull(String title) {
+        return title != null;
     }
 }
