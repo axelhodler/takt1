cf47a28cc48cfe9a859654549e75e1cd2c91c5bb
xorrr <derxor@gmail.com>
2014-02-26 18:42:38 +0100

    provide listener to createConfig method

--- src/main/java/bot/BotConfig.java
+++ src/main/java/bot/BotConfig.java
@@ -1,15 +1,17 @@
 package bot;
 
 import org.pircbotx.Configuration;
+import org.pircbotx.hooks.Listener;
 
 import bot.config.EnvironmentVars;
 
 public class BotConfig {
-    public Configuration createConfig() {
+    public Configuration createConfig(Listener listener) {
         Configuration config = new Configuration.Builder()
             .setName(System.getenv(EnvironmentVars.NAME))
             .setServerHostname(System.getenv(EnvironmentVars.SERVER))
             .addAutoJoinChannel(System.getenv(EnvironmentVars.CHANNEL))
+            .addListener(listener)
             .buildConfiguration();
         
         return config; 
--- src/test/java/bot/TestConfiguration.java
+++ src/test/java/bot/TestConfiguration.java
@@ -3,16 +3,23 @@ package bot;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.mockito.Mock;
 import org.pircbotx.Configuration;
+import org.pircbotx.PircBotX;
+import org.pircbotx.hooks.Listener;
 
 import bot.config.EnvironmentVars;
 
 public class TestConfiguration {
 
+    @Mock
+    PircBotX pircbotx;
+    
     @Test
     public void configurationComplete() {
+        Listener listener = new Bot(pircbotx);
         BotConfig conf = new BotConfig();
-        Configuration configuration = conf.createConfig();
+        Configuration configuration = conf.createConfig(listener);
 
         assertEquals(System.getenv(EnvironmentVars.NAME),
                 configuration.getName());
