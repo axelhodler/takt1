cb3e06b8ba0a4ca2a7eed072e7f88ead79d49716
xorrr <derxor@gmail.com>
2014-02-26 01:22:02 +0100

    use the env vars in the run_tests script

---
+++ bin/run_tests.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+export PORT=5000
+export NAME=test
+export SERVER=test
+export CHANNEL=test
+export IDENT=test
+export PASSWORD=test
+export WEBAPPURL=test
+export RESTAPIURL=test
+
+mvn test
