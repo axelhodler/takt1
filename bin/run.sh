3c83b247b617b83511f10c0444a9391d0ef888e2
xorrr <derxor@gmail.com>
2014-02-26 19:24:29 +0100

    add some real values to the run script to make the bot work locally

--- bin/run.sh
+++ bin/run.sh
@@ -3,5 +3,12 @@
 # Set the port for the webapp
 # only necessary when running locally
 export PORT=5000
+export NAME=takt1
+export SERVER=irc.freenode.org
+export CHANNEL=#takt1
+export IDENT=test
+export PASSWORD=test
+export WEBAPPURL=test
+export RESTAPIURL=test
 
 mvn exec:java -Dexec.mainClass="Main"
