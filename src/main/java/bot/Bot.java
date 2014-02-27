73a02bf5f42925377f31eba8da8107136467131c
xorrr <derxor@gmail.com>
2014-02-27 01:00:33 +0100

    gut Bot and remove all the handling of events stuff

--- src/main/java/bot/Bot.java
+++ src/main/java/bot/Bot.java
@@ -4,21 +4,10 @@ import java.io.IOException;
 
 import org.pircbotx.PircBotX;
 import org.pircbotx.exception.IrcException;
-import org.pircbotx.hooks.Listener;
-import org.pircbotx.hooks.ListenerAdapter;
-import org.pircbotx.hooks.events.MessageEvent;
-
-import bot.restcalls.Link;
-import bot.restcalls.RestAccessor;
 
 import com.google.inject.Inject;
 
-@SuppressWarnings("rawtypes")
-public class Bot extends ListenerAdapter implements Listener {
-
-    private TitleGrabber titleGrabber = new TitleGrabber();
-    private UrlGrabber urlGrabber = new UrlGrabber();
-
+public class Bot {
     private PircBotX pircBotX;
 
     @Inject
@@ -34,47 +23,5 @@ public class Bot extends ListenerAdapter implements Listener {
         } catch (IrcException e) {
             e.printStackTrace();
         }
-    }
-
-    @Override
-    public void onMessage(MessageEvent event) throws Exception {
-        event.getChannel().send().action(event.getMessage());
-        String url = urlGrabber.grabUrl(event.getMessage());
-        if (url != null) {
-            String title = titleGrabber.grabTitle(url);
-            sendTitleToChannelIfFound(event, title);
-
-            Link link = createLinkToSave(event, url,
-                    setTitleEmptyStringIfNotFound(title));
-            RestAccessor.getInstance().addLink(link);
-        }
-    }
-
-    private String setTitleEmptyStringIfNotFound(String title) {
-        if (title == null) {
-            title = "";
-        }
-        return title;
-    }
-
-    private void sendTitleToChannelIfFound(MessageEvent event, String title) {
-        if (isTitleExtraced(title)) {
-            sendTitleToChannel(event, title);
-        }
-    }
-
-    private Link createLinkToSave(MessageEvent event, String urlInMessage,
-            String title) {
-        Link link = new Link().setTitle(title).setUrl(urlInMessage)
-                .setUser(event.getUser().getNick());
-        return link;
-    }
-
-    private void sendTitleToChannel(MessageEvent event, String title) {
-        event.getChannel().send().action(title);
-    }
-
-    private boolean isTitleExtraced(String title) {
-        return title != null;
     }
 }
