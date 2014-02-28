1fa2d0a6028965f85eeb1fb9e8562a36e8a0a4eb
xorrr <derxor@gmail.com>
2014-02-28 03:02:04 +0100

    rename TestConfig to TestEnvVars

--- src/test/java/org/xorrr/bot/config/TestEnvVars.java
+++ src/test/java/org/xorrr/bot/config/TestEnvVars.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.xorrr.bot.config.EnvironmentVars;
 
-public class TestConfig {
+public class TestEnvVars {
 
     @Test
     public void constantsForConfigEnvVars() {
