3656104793decfc010d1beb56fc8462a6e4a4708
xorrr <derxor@gmail.com>
2014-03-01 16:56:05 +0100

    organize imports in testwebapp

--- src/test/java/org/xorrr/bot/webapp/TestWebApp.java
+++ src/test/java/org/xorrr/bot/webapp/TestWebApp.java
@@ -1,16 +1,13 @@
 package org.xorrr.bot.webapp;
 
-import static org.junit.Assert.*;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
