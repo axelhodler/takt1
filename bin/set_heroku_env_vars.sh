3ee7340a3aef3500167add5645f9106883951fe7
xorrr <derxor@gmail.com>
2014-03-01 18:54:27 +0100

    add script to help set the necessary env vars in the heroku config

---
+++ bin/set_heroku_env_vars.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Set the values of the needed environment variables, don't forget to doublecheck your input"
+
+echo "Set value for the PORT"
+read port
+heroku config:set PORT=$port
+
+echo "Set value for the NAME of the bot"
+read name
+heroku config:set NAME=$name
+
+echo "Set value for the irc SERVER (eg. irc.freenode.org)"
+read server
+heroku config:set SERVER=$server
+
+echo "Set value for the CHANNEL"
+read channel
+heroku config:set CHANNEL=$channel
+
+echo "Set value for the IDENT"
+read ident
+heroku config:set IDENT=$ident
+
+echo "Set value for the PASSWORD to the ident"
+read pw
+heroku config:set PASSWORD=$pw
