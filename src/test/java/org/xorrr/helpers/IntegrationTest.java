8aeb3c0acf360d19925c2268fcb1b1b242941435
xorrr <derxor@gmail.com>
2014-03-29 23:05:14 +0100

    add integrationtest interface to annotate integrationtests

---
+++ src/test/java/org/xorrr/helpers/IntegrationTest.java
@@ -0,0 +1,15 @@
+package org.xorrr.helpers;
+
+/**
+ * Used to exclude the integration-tests when just invoking
+ * mvn test
+ * 
+ * for running all tests (integration tests included) use:
+ * mvn integration-test
+ * 
+ * @author xorrr
+ *
+ */
+public interface IntegrationTest {
+
+}
