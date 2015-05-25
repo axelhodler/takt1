44187f6eb51734a4fa9c9a99d47819f49a46ad00
xorrr <derxor@gmail.com>
2015-05-25 19:55:44 +0200

    feat: RelayMessage will check message for http urls

--- src/main/java/org/xorrr/bot/RelayMessage.java
+++ src/main/java/org/xorrr/bot/RelayMessage.java
@@ -1,9 +1,22 @@
 package org.xorrr.bot;
 
+import org.xorrr.bot.finder.SpotifyTrackTitleFinder;
+import org.xorrr.bot.finder.SpotifyUriFinder;
+import org.xorrr.bot.finder.UrlFinder;
+
+import com.google.inject.Inject;
+
 public class RelayMessage {
 
+  private UrlFinder urlFinder;
+
+  @Inject
+  public RelayMessage(UrlFinder urlFinder) {
+    this.urlFinder = urlFinder;
+  }
+
   public void relay(String message) {
-    throw new UnsupportedOperationException();
+    urlFinder.findUrl(message);
   }
 
 }
---
+++ src/test/java/org/xorrr/bot/RelayMessageTest.java
@@ -0,0 +1,23 @@
+package org.xorrr.bot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.xorrr.bot.finder.UrlFinder;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RelayMessageTest {
+
+  @Mock
+  UrlFinder urlFinder;
+
+  @Test
+  public void checksForHttpUrls() {
+    RelayMessage messageRelais = new RelayMessage(urlFinder);
+
+    messageRelais.relay("message");
+
+    urlFinder.findUrl("message");
+  }
+}
