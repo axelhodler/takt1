e05d6216337e6ecd3345058cc1d9541d166c972a
xorrr <derxor@gmail.com>
2015-05-26 23:22:48 +0200

    feat: implement ResponseFinder logic

--- src/main/java/org/xorrr/bot/ResponseFinder.java
+++ src/main/java/org/xorrr/bot/ResponseFinder.java
@@ -1,6 +1,8 @@
 package org.xorrr.bot;
 
 import org.xorrr.bot.finder.HtmlTitleFetcher;
+import org.xorrr.bot.finder.SpotifyTrackTitleFinder;
+import org.xorrr.bot.finder.SpotifyUriFinder;
 import org.xorrr.bot.finder.UrlFinder;
 
 import com.google.inject.Inject;
@@ -9,19 +11,30 @@ public class ResponseFinder {
 
   private UrlFinder urlFinder;
   private HtmlTitleFetcher titleFinder;
+  private SpotifyUriFinder spotifyUriFinder;
+  private SpotifyTrackTitleFinder trackTitleFinder;
 
   @Inject
-  public ResponseFinder(UrlFinder urlFinder, HtmlTitleFetcher titleFinder) {
+  public ResponseFinder(UrlFinder urlFinder, HtmlTitleFetcher titleFinder,
+      SpotifyUriFinder spotifyUriFinder, SpotifyTrackTitleFinder trackTitleFinder) {
     this.urlFinder = urlFinder;
     this.titleFinder = titleFinder;
+    this.spotifyUriFinder = spotifyUriFinder;
+    this.trackTitleFinder = trackTitleFinder;
   }
 
   public String decideResponseTo(String message) {
-    String extractedUrl = urlFinder.extractUrlIn(message);
-    if (extractedUrl != null) {
-      titleFinder.fetchTitleFrom(extractedUrl);
-    };
-    return null;
+    String channelResponse = "";
+
+    if (urlFinder.urlExtractableIn(message)) {
+      String extractedUrl = urlFinder.extractUrlIn(message);
+      channelResponse = titleFinder.fetchTitleFrom(extractedUrl);
+    } else if (spotifyUriFinder.isUriExtractableIn(message)){
+      String foundUri = spotifyUriFinder.findUri(message);
+      channelResponse = trackTitleFinder.fetchTitleFrom(foundUri);
+    }
+
+    return channelResponse;
   }
 
 }
--- src/test/java/org/xorrr/bot/ResponseFinderTest.java
+++ src/test/java/org/xorrr/bot/ResponseFinderTest.java
@@ -1,15 +1,16 @@
 package org.xorrr.bot;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-
-import static org.mockito.Mockito.verify;
-import static org.mockito.BDDMockito.given;
-
 import org.mockito.runners.MockitoJUnitRunner;
 import org.xorrr.bot.finder.HtmlTitleFetcher;
+import org.xorrr.bot.finder.SpotifyTrackTitleFinder;
+import org.xorrr.bot.finder.SpotifyUriFinder;
 import org.xorrr.bot.finder.UrlFinder;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -19,28 +20,52 @@ public class ResponseFinderTest {
   UrlFinder urlFinder;
   @Mock
   HtmlTitleFetcher titleFinder;
+  @Mock
+  SpotifyUriFinder spotifyUriFinder;
+  @Mock
+  SpotifyTrackTitleFinder spotifyTrackTitleFinder;
 
   private ResponseFinder messageRelais;
 
   @Before
   public void initialize() {
-    messageRelais = new ResponseFinder(urlFinder, titleFinder);
+    messageRelais = new ResponseFinder(urlFinder, titleFinder, spotifyUriFinder, spotifyTrackTitleFinder);
   }
 
   @Test
   public void checksForHttpUrls() {
     messageRelais.decideResponseTo("message");
 
-    verify(urlFinder).extractUrlIn("message");
+    verify(urlFinder).urlExtractableIn("message");
   }
 
   @Test
   public void fetchTitleOfExtractedUrl() {
     String messageContainingAUrl = "Hi, check this out www.foobar.org";
+    given(urlFinder.urlExtractableIn(messageContainingAUrl)).willReturn(true);
     given(urlFinder.extractUrlIn(messageContainingAUrl)).willReturn("www.foobar.org");
 
     messageRelais.decideResponseTo(messageContainingAUrl);
 
     verify(titleFinder).fetchTitleFrom("www.foobar.org");
+  }
+
+  @Test
+  public void checkForSpotifyUrisInMessage() {
+    given(urlFinder.urlExtractableIn("message")).willReturn(false);
+
+    messageRelais.decideResponseTo("message");
+
+    verify(spotifyUriFinder).isUriExtractableIn("message");
+  }
+
+  @Test
+  public void getTitleOfSpotifyUriIfUriWasFound() {
+    given(urlFinder.urlExtractableIn("message")).willReturn(false);
+    given(spotifyUriFinder.isUriExtractableIn("message")).willReturn(true);
+
+    messageRelais.decideResponseTo("message");
+
+    verify(spotifyUriFinder).findUri("message");
   }
 }
